a8d5d06f3a4573b049cb92b80bd4e8d4b1ae074e
AFB <mxsh741@gmail.com>
2024-10-27 22:55:55 +0500

    chore: указаны игнорируемые eslint файлы

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,14 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"], languageOptions: {sourceType: "module"}
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    ignores: ["webpack.config.dev.js", "webpack.config.prod.js"]
+  },
   pluginJs.configs.recommended,
 ];
